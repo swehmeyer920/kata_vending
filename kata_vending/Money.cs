808a4a4cfe9d54567aab020358d13299445d4d43
swehmeyer920 <stephen.wehmeyer@gmail.com>
2018-01-16 12:50:14 -0500

    Added new Money Class and tests for it

---
+++ kata_vending/Money.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata_vending
+{
+    public class Money
+    {
+        public enum Coin
+        {
+            invalid = 0,
+            nickel = 1,
+            dime = 2,
+            quarter = 3
+        }
+
+        public static decimal GetValue(Coin coin)
+        {
+            switch (coin)
+            {
+                case Coin.invalid:
+                    break;
+                case Coin.nickel:
+                    return 5M;
+                case Coin.dime:
+                    return 10M;
+                case Coin.quarter:
+                    return 25M;
+            }
+            return 0M;
+        }
+    }
+}
---
+++ kata_vendingTests/MoneyTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kata_vending;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata_vending.Tests
+{
+    [TestClass()]
+    public class MoneyTests
+    {
+        [TestMethod()]
+        public void getValueTest()
+        {
+            Assert.AreEqual(5m, Money.GetValue(Money.Coin.nickel));
+            Assert.AreEqual(10m, Money.GetValue(Money.Coin.dime));
+            Assert.AreEqual(25m, Money.GetValue(Money.Coin.quarter));
+            Assert.AreEqual(0m, Money.GetValue(Money.Coin.invalid));
+        }
+    }
+}
