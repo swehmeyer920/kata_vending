a8132a19059bfd7e7d5812e64f435609e19bc38b
swehmeyer920 <stephen.wehmeyer@gmail.com>
2018-01-16 14:29:51 -0500

    Added vending machine and test for accepting nickels, dimes, or quarters

---
+++ kata_vending/VendingMachine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata_vending
+{
+    public class VendingMachine
+    {
+        public bool acceptCoin(Money.Coin coin)
+        {
+            if (coin == Money.Coin.nickel)
+                return true;
+            if (coin == Money.Coin.dime)
+                return true;
+            if (coin == Money.Coin.quarter)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ kata_vendingTests/VendingMachineTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kata_vending;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata_vending.Tests
+{
+    [TestClass()]
+    public class VendingMachineTests
+    {
+        [TestMethod()]
+        public void acceptCoinTest()
+        {
+            VendingMachine machine = new VendingMachine();
+            Assert.IsTrue(machine.acceptCoin(Money.Coin.nickel));
+            Assert.IsTrue(machine.acceptCoin(Money.Coin.dime));
+            Assert.IsTrue(machine.acceptCoin(Money.Coin.quarter));
+            Assert.IsFalse(machine.acceptCoin(Money.Coin.invalid));
+        }
+    }
+}
